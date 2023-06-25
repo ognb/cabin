e117875288f5d50b01036104d09eaa77a9680983
O.Ganbat <o.ganbats@gmail.com>
2023-06-25 20:24:57 +0900

    implemented login feature

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -3,12 +3,18 @@ import Button from "../../ui/Button";
 import Form from "../../ui/Form";
 import Input from "../../ui/Input";
 import FormRowVertical from "../../ui/FormRowVertical";
+import { useLogin } from "./useLogin";
 
 function LoginForm() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const { login, isLoading } = useLogin();
 
-  function handleSubmit() {}
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!email || !password) return;
+    login({ email, password });
+  }
 
   return (
     <Form onSubmit={handleSubmit}>
@@ -20,8 +26,10 @@ function LoginForm() {
           autoComplete="username"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
+          disabled={isLoading}
         />
       </FormRowVertical>
+
       <FormRowVertical label="Password">
         <Input
           type="password"
@@ -29,10 +37,14 @@ function LoginForm() {
           autoComplete="current-password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
+          disabled={isLoading}
         />
       </FormRowVertical>
       <FormRowVertical>
-        <Button size="large">Log in</Button>
+        <Button size="large" disabled={isLoading}>
+          {/* {!isLoading ? "Log in" : <SpinnerMini />} */}
+          Log in
+        </Button>
       </FormRowVertical>
     </Form>
   );
--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -1,10 +1,10 @@
-// import supabase from "./supabase";
+import supabase from "./supabase";
 
-// export async function loginApi({ email, password }) {
-//   let { data, error } = await supabase.auth.signInWithPassword({
-//     email,
-//     password,
-//   });
-//   if (error) throw new Error(error.message);
-//   return data;
-// }
+export async function loginApi({ email, password }) {
+  let { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
+  if (error) throw new Error(error.message);
+  return data;
+}
