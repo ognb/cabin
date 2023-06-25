8accd56eb238db9b8e98a3e0b2dfdd91e2597822
O.Ganbat <o.ganbats@gmail.com>
2023-06-25 22:10:41 +0900

    implemented router protection

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,20 +1,21 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { loginApi } from "../../services/apiAuth";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-hot-toast";
 
 export function useLogin() {
+  const queryClient = useQueryClient();
   const navigate = useNavigate();
+
   const { mutate: login, isLoading } = useMutation({
     mutationFn: ({ email, password }) => loginApi({ email, password }),
     onSuccess: (user) => {
-      console.log(user);
-
-      navigate("/dashboard", { replace: true });
+      queryClient.setQueryData(['user'], user.user);
+      navigate('/dashboard', { replace: true });
     },
-    onError: (error) => {
-      console.error(error);
-      toast.error("Provided email or password are incorrect");
+    onError: (err) => {
+      console.log('ERROR', err);
+      toast.error('Provided email or password are incorrect');
     },
   });
 
